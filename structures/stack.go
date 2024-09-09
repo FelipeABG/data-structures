1a95b27607785928bda1b32b6ef0e2f371e7e43a
Felipe Augusto <felipe.baleche2@gmail.com>
2024-09-09 12:00:57 -0300

    added remove and insert methods implementation in stack

--- structures/stack.go
+++ structures/stack.go
@@ -1,5 +1,7 @@
 package structures
 
+import "errors"
+
 type stack[T any] struct {
 	top  int8
 	size int8
@@ -15,19 +17,36 @@ func NewStack[T any](size int8) *stack[T] {
 }
 
 func (s stack[T]) Empty() bool {
-
+	return s.top == -1
 }
 
 func (s *stack[T]) Full() bool {
-
+	return s.top == s.size-1
 }
 
-func (s *stack[T]) Insert(element T) {
+func (s *stack[T]) Insert(element T) error {
 
+	if s.Full() {
+		return errors.New("ERROR: Attempt to insert an element into a full stack")
+	}
+
+	s.top++
+	s.data[s.top] = element
+
+	return nil
 }
 
-func (s *stack[T]) Remove() T {
+func (s *stack[T]) Remove() (T, error) {
 
+	if s.Empty() {
+		return *new(T), errors.New("ERROR: Attempt to remove an element from an empty stack")
+	}
+
+	target := s.data[s.top]
+	s.data[s.top] = *new(T)
+	s.top--
+
+	return target, nil
 }
 
 func (s stack[T]) Peek() T {
